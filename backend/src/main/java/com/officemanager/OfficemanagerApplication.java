a2bfc3330f5f5d81203bbc731f5729c24d9e2601
Richard Nduka <koolrich@gmail.com>
2017-03-05 15:37:01 +0000

    Initial commit - project structure

---
+++ backend/src/main/java/com/officemanager/OfficemanagerApplication.java
@@ -0,0 +1,12 @@
+package com.officemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficemanagerApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/officemanager/OfficemanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.officemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OfficemanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
